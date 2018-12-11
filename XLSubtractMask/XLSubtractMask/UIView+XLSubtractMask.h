ccf1beeef1f648ca066a3a1cf95687d1df715928
Mac-Qke <mac-qke@Mac-QkedeMac-mini.local>
2018-12-11 16:55:36 +0800

    完善代码

--- XLSubtractMask/XLSubtractMask/UIView+XLSubtractMask.h
+++ XLSubtractMask/XLSubtractMask/UIView+XLSubtractMask.h
@@ -11,6 +11,21 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView (XLSubtractMask)
+/**
+设置镂空遮罩视图，该方法本质上是设置maskView
+如果寄宿图的内容有更新，需要手动再调用setter方法
+
+@param view 遮罩视图
+ */
+- (void)setSubtractMaskView:(UIView *)view;
+
+/**
+ 获取镂空遮罩视图，用于动态修改遮罩的一些属性
+ 
+ @return 遮罩视图
+ */
+
+- (UIView *)subtractMaskView;
 
 @end
 
--- XLSubtractMask/XLSubtractMask/ViewController.m
+++ XLSubtractMask/XLSubtractMask/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import "UIView+XLSubtractMask.h"
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *blackView;
+@property (weak, nonatomic) IBOutlet UIView *visualEffectContainer;
 
 @end
 
@@ -19,5 +21,37 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)actionScale:(UIButton *)sender {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.blackView.subtractMaskView.transform = CGAffineTransformMakeScale(2, 2);
+        self.visualEffectContainer.subtractMaskView.transform = CGAffineTransformMakeScale(2, 2);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.5 animations:^{
+            self.blackView.subtractMaskView.transform = CGAffineTransformIdentity;
+            self.visualEffectContainer.subtractMaskView.transform = CGAffineTransformIdentity;
+        }];
+    }];
+}
+
+- (IBAction)actionImage:(UIButton *)sender {
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pikaqiu.png"]];
+    imageView.frame = CGRectMake(30, 20, 100, 95);
+    
+    self.blackView.subtractMaskView = imageView;
+    self.visualEffectContainer.subtractMaskView = imageView;
+    
+}
+
+- (IBAction)actionText:(UIButton *)sender {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 240, 128)];
+    label.textColor = [UIColor whiteColor];
+    label.font = [UIFont systemFontOfSize:24];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = @"XLSubtractMask";
+    
+    self.blackView.subtractMaskView = label;
+    self.visualEffectContainer.subtractMaskView = label;
+    
+}
 
 @end
